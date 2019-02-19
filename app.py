ead4454ff26f99125906b541f2f6656df8afb3fa
Jacob Danovitch <jacobgdt@gmail.com>
2019-02-18 23:16:31 -0500

    fix 404 msg

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def api(query, model):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),
+    return render_template('404.html'), 404
 
 
 @app.route("/about")
